aaadf4bbe33f26e78eb5a6b4591f9567ace7bd83
Hamilton Carlos da Silva Santos <hamilton.s.santos@hotmail.com>
2016-04-29 11:23:10 -0300

    tuto almost done

--- WatchPong/Controllers/TutorialContentViewController.swift
+++ WatchPong/Controllers/TutorialContentViewController.swift
@@ -12,16 +12,47 @@ class TutorialContentViewController: UIViewController {
     
     var index = 0
     var imageName = ""
+    var tutoTitle = ""
+    var tutoSubtitle = ""
     
     @IBOutlet weak var contentImageView: UIImageView!
+    @IBOutlet weak var handImageView: UIImageView!
+    @IBOutlet weak var handCenterConstraint: NSLayoutConstraint!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.contentImageView.image = UIImage(named: imageName)
-
+        handImageView.alpha = 0
+        animateHand(1)
+        handCenterConstraint.constant = 0
+        contentImageView.image = UIImage(named: imageName)
+        titleLabel.text = tutoTitle
+        subtitleLabel.text = tutoSubtitle
+        if index == 2 {
+            handImageView.alpha = 1
+        }
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        
+    }
+    
+    func animateHand(interval: NSTimeInterval) {
+        UIView.animateWithDuration(1, animations: {
+            self.handCenterConstraint.constant = self.view.frame.size.width/4
+            self.view.layoutIfNeeded()
+        }) { (finished) in
+            UIView.animateWithDuration(1.0, animations: {
+                self.handCenterConstraint.constant = -(self.view.frame.size.width/4)
+                self.view.layoutIfNeeded()
+            }) { (finished) in
+                self.animateHand(2)
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
